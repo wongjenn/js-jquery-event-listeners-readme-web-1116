340abd857e453d6bffca49a540beb9ff843df2f7
wongjenn <wongjenn@yahoo.com>
2017-01-18 17:52:46 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -1,6 +1,38 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert("Hey!")
+  })
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt(){
+  $(document).on('keydown', function(key){
+    if(key.which == 71){
+      alert('g was pressed')
+      return
+    }
+  })
+}
+
+function submitIt(){
+  $("form").on("submit", function(){
+    alert('Your form is going to be submitted now.')
+    return
+  })
+}
 
 $(document).ready(function(){
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
+
 
 // call functions here
 
